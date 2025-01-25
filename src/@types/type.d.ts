51473a11701462e862d526bbbad6bef66a0f9e9f
Carlos Danyel <carlosdanyelsilva27@gmail.com>
2025-01-25 03:49:58 -0300

    feat: create project

---
+++ src/@types/type.d.ts
@@ -0,0 +1,10 @@
+type User = {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+};
+
+type UserInfo = {
+  user: User;
+};
---
+++ src/components/ui/input/field.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { ComponentProps, ReactNode } from "react";
+import { Controller, Control } from "react-hook-form";
+import { Input } from ".";
+import { FieldWrapper } from "../field-wrapper";
+
+type InputFieldProps = ComponentProps<typeof Input> & {
+  label: string;
+  name: string;
+  containerClassName?: string;
+  extraContent?: (value: string) => ReactNode;
+  control?: Control<any, any>;
+};
+
+const InputField = ({
+  label,
+  name,
+
+  required,
+  containerClassName,
+  extraContent,
+  control,
+  ...props
+}: InputFieldProps) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={{
+        required: required && "Campo obrigatorio*",
+      }}
+      render={({ field, fieldState }) => (
+        <FieldWrapper label={label} className={containerClassName}>
+          <Input {...props} {...field} value={field.value || ""} />
+          {extraContent && extraContent(field.value)}
+          {fieldState.error && (
+            <p className=" text-sm text-red-500 top-16">
+              {fieldState.error.message}
+            </p>
+          )}
+        </FieldWrapper>
+      )}
+    />
+  );
+};
+
+export default InputField;
